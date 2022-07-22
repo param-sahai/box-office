a6237c0677477b6d18c78248f89add645a365760
Param Sahai <sahai.param.2002@gmail.com>
2022-07-22 12:45:34 +0530

    Added Routing of Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>Hello there!</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is Home page
+      </Route>
+
+      <Route exact path="/starred">
+        This is Starred!
+      </Route>
+
+      <Route>404 Not Found :/</Route>
+    </Switch>
+  );
 }
 
 export default App;
